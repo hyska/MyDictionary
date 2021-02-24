8774c586f433cc0e07cf535f13b3fe9667ff831f
hyska <hayriyeyasak1@gmail.com>
2021-02-24 17:31:51 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+
+            values = new TValue[0];
+        }      
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for(int i=0;i<tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+        }
+        public void Get()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", keys[i], values[i]);
+            }
+        }
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary1 = new Dictionary<int,string>();
+            dictionary1.Add(1, "deneme");           
+
+            foreach(var keyValue in dictionary1)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", keyValue.Key, keyValue.Value);
+            }
+            Console.WriteLine("~~~~ My Dictionary~~~~");
+            var mydictionary = new MyDictionary<int, string>();
+            mydictionary.Add(2, "Deneme2");
+            mydictionary.Get();
+            
+
+        }
+    }
+}
